c6d20fab75cec97628ef4f72b4818728047d2178
jennyhalo <jenny.halonen@student.laurea.fi>
2022-10-24 10:59:20 +0300

    init

---
+++ script.js
@@ -0,0 +1,26 @@
+// Create a "close" button and append it to each list item
+
+
+// Click on a close button to hide the current list item
+
+
+// Add a "checked" symbol when clicking on a list item
+
+
+// Create a new list item when clicking on the "Lisää" button
+function addButton() {
+
+
+}
+// Select all items when clicking the "Kaikki" button
+function selectAllBtn() {
+
+}
+// Select the active items when clicking the "Hakematta" button
+function selectActiveBtn() {
+
+}
+// Select the completed items when clicking the "Noudetut" button
+function selectCompletedBtn() {
+
+}
